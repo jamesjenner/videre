c83e9a83757c9b3b35894e4b49d0dba50473180c
James Jenner <james.g.jenner@gmail.com>
2012-10-28 18:17:41 +1000

    Added logic for settings within tabs for the content manager, with initial implementation in index.html

--- assets/js/contentmanager.js
+++ assets/js/contentmanager.js
@@ -26,8 +26,11 @@ ContentManager.prototype = {
     addPane: function(id) {
         this.panes[id] = new Pane();
     },
-    addTab: function(pane, options) {
-        // do we really need this?
+    addTab: function(paneId, tabId, settings) {
+        this.panes[paneId].tabs[tabId] = new Tab(settings);
+    },
+    addGauge: function(pane, gaugeId, gauge) {
+        this.panes[id].gauges[gaugeId] = gauge;
     },
     setFocusedTabId: function(paneId, tabId) {
         this.panes[paneId].focusedTabId = tabId;
@@ -37,14 +40,80 @@ ContentManager.prototype = {
         
         return this.panes[paneId].focusedTabId;
     },
-    setFocusedTab: function(paneId, nbr) {
+    setFocusedTabNbr: function(paneId, nbr) {
         this.panes[paneId].focusedTab = nbr;
     },
-    getFocusedTab: function(paneId) {
+    getFocusedTabNbr: function(paneId) {
+        paneId = (typeof paneId === "undefined") ? this.focusedPaneId : paneId;
+        
         return this.panes[paneId].focusedTab;
+    },
+    getFocusedTab: function(paneId) {
+        paneId = (typeof paneId === "undefined") ? this.focusedPaneId : paneId;
+        
+        return this.panes[paneId].tabs[this.getFocusedTabId(paneId)];
+    },
+    getFocusedTabSettings: function(paneId) {
+        paneId = (typeof paneId === "undefined") ? this.focusedPaneId : paneId;
+        if(typeof this.panes[paneId].tabs[this.getFocusedTabId(paneId)].settings === "undefined") {
+            return undefined;
+        } else {
+            return this.panes[paneId].tabs[this.getFocusedTabId(paneId)].settings.settings;
+        }
     },
 };
 
 var Pane = function (options) {
     this.currentTab = 0;
+    this.tabs = new Object();
+}
+
+var Tab = function (settings) {
+    this.settings = settings;
+}
+
+var TabSettings = function() {
+    this.settings = new Array();
+}
+
+TabSettings.prototype = {
+    add: function(setting) {
+        this.settings[this.settings.length] = setting;
+    },
+};
+
+
+var GaugeSetting = function (id, gauge, options) {
+    if(!id) return null;
+    if(!gauge) return null;
+    
+    this.id = id;
+    this.gauge = gauge;
+    
+    this.options = options || {};
+
+    this.currentValue = this.options.currentValue || 0;
+    this.x = this.options.x || 0;
+    this.y = this.options.y || 0;
+    this.visible = this.options.visible || false;
+}
+
+var HeadingSetting = function (title) {
+    this.title = title || 'undefined';
+}
+
+var ToggleSetting = function (id, label, value) {
+    if(!id) return null;
+    
+    this.id = id;
+    this.value = value || false;
+    this.label = label || 'undefined';
+}
+
+var TextSetting = function (id, label, value) {
+    if(!id) return null;
+    
+    this.id = id;
+    this.value = value || '';
+    this.label = label || 'undefined';
 }
